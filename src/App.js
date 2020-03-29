2e6ccd97da9213a7d23d389a147d8abaea707b5f
STR <saturasstotenkopf@gmail.com>
2020-03-29 22:14:31 +0700

    Tugas 11 Semantic UI
    Mengumpulkan tugas 11 Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Segment,Grid,Header,Icon,Search,Divider,Button,Placeholder,Dimmer,Loader,List} from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Segment placeholder>
+        <Grid columns={2} stackable textAlign='center'>
+          <Divider vertical>Or</Divider>
+          <Grid.Row>
+            <Grid.Column>
+              <Header icon>
+                <Icon name='search'/>
+                Cari Dokumen
+              </Header>
+              <Search placeholder='Search Document' />
+            </Grid.Column>
+            <Grid.Column>
+              <Header icon>
+                <Icon name='pdf file outline'/>
+                Tambah Dokumen Baru
+              </Header>
+              <Button primary>
+                Create Document
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment>
+        <Placeholder>
+          <Placeholder.Paragraph>
+            <Placeholder.Line/>
+            <Placeholder.Line/>
+            <Placeholder.Line/>
+          </Placeholder.Paragraph>
+        </Placeholder>
+        <Dimmer active>
+          <Loader content='loading'/>
+        </Dimmer>
+      </Segment>
+
+      <Grid columns='equal'>
+        <Grid.Row>
+          <Grid.Column width={5}>
+            <Segment>
+              <Placeholder>
+                <Placeholder.Header image>
+                  <Placeholder.Line/>
+                  <Placeholder.Line/>
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line/>
+                  <Placeholder.Line/>
+                </Placeholder.Paragraph>
+              </Placeholder>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Segment>
+              <Placeholder>
+                <Placeholder.Header image>
+                  <Placeholder.Line/>
+                  <Placeholder.Line/>
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line/>
+                  <Placeholder.Line/>
+                </Placeholder.Paragraph>
+              </Placeholder>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h2' content='Website Terkait'/>
+            <List>
+              <List.Item icon='linkify' content={<a href='google.com'>Google</a>}/>
+              <List.Item icon='linkify' content={<a href='facebook.com'>Facebook</a>}/>
+              <List.Item icon='linkify' content={<a href='https://react.semantic-ui.com/'>Semantic-UI</a>}/>
+              <List.Item icon='linkify' content={<a href='niomic.com'>Niomic</a>}/>
+              <List.Item icon='linkify' content={<a href='https://reactjs.org/'>React</a>}/>
+            </List>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
     </div>
   );
 }
